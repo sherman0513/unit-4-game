8b3e1c46f96eb78fc8a7d3943faa8bbef604f372
unknown <sherman.jonathan.r@gmail.com>
2019-03-10 21:34:18 -0700

    finished game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,111 @@
+//sets up random number user is trying to match
 $(document).ready(function () {
-    var targetNumber = Math.floor(Math.random() * 100);
+    var Random = Math.floor(Math.random() * 101 + 19)
+    console.log(Random)
+    $('#number').text(Random);
 
-    $("#number-to-guess").text(targetNumber);
-    console.log(targetNumber);
-
-    var counter = 0;
-    var numberOptions = [3, 6, 14, 11];
+    //	setting up four random numbers array, total variable and printing the wins & losses variables	
+    var GeneratedNumbers = []
+    var total = 0;
     var wins = 0;
     var losses = 0;
+    $('#numberWins').text(wins);
+    $('#numberLosses').text(losses);
 
-    function reset() {
-        targetNumber = "";
-        wins = "";
-        losses = "";
-    }
 
-    for (var i = 0; i < numberOptions.length; i++) {
-        var imageLego = $("<img>");
-        imageLego.addClass("lego-image");
-        imageLego.attr("src", "assets/images/legos.jpg")
-        imageLego.attr("data-legovalue", numberOptions[i]);
-        $("#legos").append(imageLego);
-        console.log(imageLego);
-    }
-
-    $(".lego-image").on("click", function () {
-        var legoValue = ($(this).attr("data-legovalue"));
-        legoValue = parseInt(legoValue);
-        counter += legoValue;
-
-        $("#total-score").text(counter);
-
-        if (counter === targetNumber) {
-            wins++
-            $("#wins").text(wins);
-            reset();
+    //sets up random numbers for each jewel
+    function randomNumbers() {
+        for (var i = 0; i < 4; i++) {
+            var num = Math.floor(Math.random() * 11 + 1);
+            GeneratedNumbers.push(num);
         }
+        // console.log(GeneratedNumbers)
+    }
 
-        else if (counter > targetNumber) {
-            losses++
-            $("#losses").text(losses);
-            reset();
+    randomNumbers();
+
+
+    //resets the game after player wins or loses
+    function reset() {
+        Random = Math.floor(Math.random() * 101 + 19);
+        // console.log(Random)
+        $('#number').text(Random);
+        GeneratedNumbers = [];
+        randomNumbers();
+        total = 0;
+        $('#Combined').text(total);
+    }
+
+    //adds the wins to the total after each round is played
+    function win() {
+        alert("You won!!!");
+        wins++;
+        $('#numberWins').text(wins);
+        reset();
+    }
+
+    //adds the losses to the total after each round is played
+    function lose() {
+        alert("You lose!!");
+        losses++;
+        $('#numberLosses').text(losses);
+        reset();
+    }
+
+
+
+    //sets up clicks for each time a lego picture is clicked on
+    $('#one').on('click', function () {
+        total = total + GeneratedNumbers[0];
+        // console.log("New total= " + total);
+        $('#Combined').text(total);
+
+        //sets win/lose conditions
+        if (total == Random) {
+            win();
+        }
+        else if (total > Random) {
+            lose();
+        }
+    })
+
+    $('#two').on('click', function () {
+        total = total + GeneratedNumbers[1];
+        // console.log("New total= " + total);
+        $('#Combined').text(total);
+
+        if (total == Random) {
+            win();
+        }
+        else if (total > Random) {
+            lose();
+        }
+    })
+
+    $('#three').on('click', function () {
+        total = total + GeneratedNumbers[2];
+        // console.log("New total= " + total);
+        $('#Combined').text(total);
+
+        if (total == Random) {
+            win();
+        }
+        else if (total > Random) {
+            lose();
+        }
+    })
+
+    $('#four').on('click', function () {
+        total = total + GeneratedNumbers[3];
+        // console.log("New total= " + total);
+        $('#Combined').text(total);
+
+        if (total == Random) {
+            win();
+        }
+        else if (total > Random) {
+            lose();
         }
     });
 
-});
+});	
